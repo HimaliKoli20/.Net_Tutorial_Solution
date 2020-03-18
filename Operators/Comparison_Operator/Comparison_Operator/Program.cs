6333bf81fa6d814cb5c96997fa06ec26c9f923b1
himalikoli20 <himalikoli208@gmail.com>
2020-03-18 20:20:15 +0530

    Added Operators Task

---
+++ Operators/Comparison_Operator/Comparison_Operator/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Comparison_Operator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 4, b = 2;
+            Console.WriteLine("a= {0}    b= {1} ", a, b);
+            if (a == b)
+            {
+                Console.WriteLine("a and b are Equal");
+            }
+            if (a != b)
+            {
+                Console.WriteLine("a and b are Not Equal");
+            }
+            if (a > b) 
+            {
+                Console.WriteLine("Value of a is greater than b ");
+            }
+            if (a < b)
+            {
+                Console.WriteLine("Value of b is greater than a ");
+            }
+        }
+    }
+}
---
+++ Operators/Other_Operators/Other_Operators/Other_Operators/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Other_Operators
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Conditional Operator
+            int a = 5, b = 7;
+            if(a==5 && b==10)
+            {
+                Console.WriteLine("Both conditions are true");
+            }
+            if(a == 5 || b == 10)
+            {
+                Console.WriteLine("Any one or both conditions are true");
+            }
+
+            // Ternary Operator
+            int num = 15;
+            bool Isnum15 = num == 15 ? true : false;
+            Console.WriteLine("Number == 15 is {0} ", Isnum15);
+        }
+    }
+}
