06046bce41388abaf05bfa78207a8d5ca2b7ccbd
himalikoli20 <himalikoli208@gmail.com>
2020-03-18 18:23:02 +0530

    Added String Task

---
+++ String/String/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace String
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = " Himali";
+            string my_name = " \"Himali\"";
+            string folder = @" C:\Users\DELL\Desktop";
+            Console.WriteLine(name);
+            Console.WriteLine(my_name);
+            Console.WriteLine(folder);
+        }
+    }
+}
