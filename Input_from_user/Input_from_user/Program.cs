1b76e17436deb7b03fbafc6a8478296d9d9290a5
himalikoli20 <himalikoli208@gmail.com>
2020-03-18 16:22:48 +0530

    Added Input_from_user Task

---
+++ Input_from_user/Input_from_user/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Input_from_user
+{
+    class program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter your first name ");
+            string firstname = Console.ReadLine();
+            Console.WriteLine("Enter your last name ");
+            string lastname = Console.ReadLine();
+            Console.WriteLine("Hello {0} {1}", firstname, lastname);
+        }
+    }
+}
