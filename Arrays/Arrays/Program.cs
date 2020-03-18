4b3a226d2246003b00953830fde87db77c022355
himalikoli20 <himalikoli208@gmail.com>
2020-03-18 23:14:05 +0530

    Added Arrays Task

---
+++ Arrays/Arrays/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Arrays
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] num = new int[3];
+
+            num[0] = 10;
+            num[1] = 20;
+            num[2] = 30;
+            Console.WriteLine(num[1]);
+        }
+    }
+}
