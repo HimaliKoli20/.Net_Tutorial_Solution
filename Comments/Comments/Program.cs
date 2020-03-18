028def3e72e927fe11f9e0ad394120924702b4ea
himalikoli20 <himalikoli208@gmail.com>
2020-03-18 23:33:19 +0530

    Added Comments Task

---
+++ Comments/Comments/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Comments
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //single line comment
+            Console.WriteLine("Hello World!");
+            /*
+             * This is multi line comment
+             * int a=4 , b=6;
+             * Console.WriteLine("{0}", a+b);
+             */
+
+            sample
+        }
+        /// <summary>
+        /// This is XML comments used to give summary of class 
+        /// </summary>
+        public class sample
+        {
+
+        }
+    }
+}
