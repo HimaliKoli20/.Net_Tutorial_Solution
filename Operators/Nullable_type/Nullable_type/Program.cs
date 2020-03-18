e1f79a4fa7583820513261a58676fa30c207ed28
himalikoli20 <himalikoli208@gmail.com>
2020-03-18 21:00:13 +0530

    Added Operators Task

---
+++ Operators/Nullable_type/Nullable_type/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Nullable_type
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int no_of_students = 100;
+            int? failed_stud = null;
+            if(failed_stud==null)
+            {
+                Console.WriteLine("No Student is failed");
+            }
+            else
+            {
+                Console.WriteLine("Some Students are failed");
+            }
+        }
+    }
+}
