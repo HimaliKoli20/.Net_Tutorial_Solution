dabfdf8f0642ceda40cae49a66e63efa034acfad
himalikoli20 <himalikoli208@gmail.com>
2020-03-20 00:52:45 +0530

    Added Class Task

---
+++ Class/Class_part1/Class_part1/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Class
+{
+    class Student
+    {
+        string _name;
+        string _rank;
+        public Student(string Name, string Rank)
+        {
+            this._name = Name;
+            this._rank = Rank;
+        }
+        public void Print()
+        {
+            Console.WriteLine("Miss {0}", this._name + " has got " + this._rank +" rank" );
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Student S = new Student("Richa", "First");
+            S.Print();
+        }
+    }
+}
---
+++ Class/Class_part2/Class_part2/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Class_part2
+{
+    class Circle
+    {
+        static float _pi = 3.141F;
+        int _radius;
+        public Circle(int Radius)
+        {
+            this._radius = Radius;
+        }
+        public float Area()
+        {
+            return Circle._pi * this._radius * this._radius;
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Circle C = new Circle(5);
+            float area = C.Area();
+            Console.WriteLine("Area = {0}", area);
+        }
+    }
+}
