9d0fe4483900980e00aad3ac815e7d237faccedf
himalikoli20 <himalikoli208@gmail.com>
2020-03-19 14:52:04 +0530

    Added While_loop Task

---
+++ While_loop/While_loop/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace While_loop
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter a target value");
+            int num = int.Parse(Console.ReadLine());
+            int even = 0;
+            while(even <= num)
+            {
+                if(even%2==0)
+                {
+                    Console.Write("{0}  ", even);                  
+                }
+                even ++;
+            }
+        }
+    }
+}
