8ce6cf4a7bfbaaaf7c961afe42a35e4b31f541b3
himalikoli20 <himalikoli208@gmail.com>
2020-03-20 17:01:50 +0530

    Added Polymorphism Task

---
+++ Polymorphism/Polymorphism/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace Polymorphism
+{
+    class Bollywood
+    {
+        public string movie = "Baaghi";
+        public virtual void Print()
+        {
+            Console.WriteLine(movie);
+        }
+    }
+    class Action_film : Bollywood
+    {
+        public override void Print()
+        {
+            Console.WriteLine(movie+ " 2");
+        }
+    }
+    class Comedy_movie : Bollywood
+    {
+        public override void Print()
+        {
+            Console.WriteLine(movie + " 3");
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Bollywood[] films = new Bollywood[3];
+
+            films[0] = new Bollywood();
+            films[1] = new Comedy_movie();
+            films[2] = new Action_film();
+
+            foreach(Bollywood i in films)
+            {
+                i.Print();
+            }
+        }
+    }
+}
