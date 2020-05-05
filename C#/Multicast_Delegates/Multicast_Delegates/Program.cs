2cc41111c4c2fe8da02392d32f4038584e3e716c
himalikoli20 <himalikoli208@gmail.com>
2020-05-05 23:26:54 +0530

    Task Added

---
+++ C#/Multicast_Delegates/Multicast_Delegates/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace Multicast_Delegates
+{
+    //Points to more than one function
+    //If return type is other that void then delegate returns the value of the last invoked method
+    //It makes implimentation of observer design pattern(publish/subscribe) very simple
+    public delegate void Sample();
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Sample sam1, sam2, sam3 , sam4; 
+            sam1 = new Sample(PrintMethod1);
+            sam2 = new Sample(PrintMethod2);
+            sam3 = new Sample(PrintMethod3);
+
+            sam4 = sam1 + sam2 + sam3;
+            //sam4 = sam1 + sam2 + sam3 - sam2;
+            //sam4 += sam1;
+
+            sam4();
+        }
+
+        public static void PrintMethod1()
+        {
+            Console.WriteLine("PrintMethod1 Invoked");
+        }
+        public static void PrintMethod2()
+        {
+            Console.WriteLine("PrintMethod2 Invoked");
+        }
+        public static void PrintMethod3()
+        {
+            Console.WriteLine("PrintMethod3 Invoked");
+        }
+
+    }
+}
