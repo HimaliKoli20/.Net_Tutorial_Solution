97715dd85779d78dc7b195d997e2cee903d2064e
himalikoli20 <himalikoli208@gmail.com>
2020-05-05 21:56:13 +0530

    Task Added

---
+++ C#/Delegate_Usage/Delegate_Usage/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Delegate_Usage
+{ 
+    //A function can be passed as a parameter using delegates
+   
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Employee> emplist = new List<Employee>();
+
+            emplist.Add(new Employee() { Id = 1, Name = "Sara", Salary = 1000, Experience = 2 });
+            emplist.Add(new Employee() { Id = 2, Name = "Harsh", Salary = 2000, Experience = 1 });
+            emplist.Add(new Employee() { Id = 3, Name = "Dhriti", Salary = 3000, Experience = 5 });
+            emplist.Add(new Employee() { Id = 4, Name = "Viraj", Salary = 4000, Experience = 7 });
+          
+
+            Employee.PromoteEmp(emplist , emp => emp.Experience>=3);
+        }
+
+        
+        delegate bool IsPromotable(Employee empl);
+
+        class Employee
+        {
+            public int Id { get; set; }
+            public string Name { get; set; }
+            public int Salary { get; set; }
+            public int Experience { get; set; }
+
+            public static void PromoteEmp(List<Employee> emplist, IsPromotable is_Promotable)
+            {
+                foreach (Employee employee in emplist)
+                {
+                    if (is_Promotable(employee))
+                    {
+                        Console.WriteLine("Employee {0} gets promoted ",employee.Name);
+                    }
+                }
+            }
+
+        }
+    }
+}
---
+++ C#/Delegates/Delegates/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Delegates
+{
+    // A delegate is a type safe function pointer
+    public delegate void HelloDelegate(string msg); //  This delegate will point to any method having same return type and takes string parameter
+
+    class Program
+    {
+        public static void Hello(string smsg)
+        {
+            Console.WriteLine(smsg);
+        }
+        public static void Main(string[] args)
+        {
+            HelloDelegate H_Del = new HelloDelegate(Hello);
+            H_Del("Hey there!");
+           // Hello("Heyyy");
+        }
+    }
+}
