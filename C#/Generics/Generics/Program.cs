0825e50281c9453e7174370836b8c8b0fb4f398b
himalikoli20 <himalikoli208@gmail.com>
2020-05-07 00:44:28 +0530

    Task Added

---
+++ C#/Generics/Generics/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Generics
+
+{
+    //It makes code independent of data type
+    //Code resuability
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool AreTheyEqual = Calculator.AreEqual<string>("A", "B");
+            if (AreTheyEqual)
+            {
+                Console.WriteLine("Equal");
+            }
+            else
+            {
+                Console.WriteLine("Not Equal");
+            }
+
+        }
+    }
+    public class Calculator
+    {
+        public static bool AreEqual<T>(T Value1, T Value2)    //T stands for Data type
+        {
+            return Value1.Equals(Value2);
+        }
+    }
+
+}
