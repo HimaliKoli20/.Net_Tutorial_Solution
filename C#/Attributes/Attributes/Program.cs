8592408fd901580afd38e2829904045158ecb0c5
himalikoli20 <himalikoli208@gmail.com>
2020-05-06 22:52:56 +0530

    Task Added

---
+++ C#/Attributes/Attributes/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.Design;
+using System.Diagnostics.CodeAnalysis;
+
+namespace Attributes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Calculator.Add(10, 20);
+            int Sum = Calculator.Add(new List<int>() { 10, 20, 30 });
+            Console.WriteLine(Sum);
+        }
+    }
+    public class Calculator
+    {
+        [Obsolete]
+        public static int Add(int FN , int SN)
+        {
+            return FN + SN;
+        }
+        public static int Add(List<int> Numbers)
+        {
+            int sum=0;
+            foreach (int Number in Numbers)
+            {
+                sum = sum + Number;
+            }
+            return sum;
+        }
+    }
+}
