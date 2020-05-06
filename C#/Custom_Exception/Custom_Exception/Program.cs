e0c28eaf7d91ca61f44aba69d2b928580d04e266
himalikoli20 <himalikoli208@gmail.com>
2020-05-06 15:45:54 +0530

    Task Added

---
+++ C#/Custom_Exception/Custom_Exception/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.IO;
+using System.Runtime.Serialization;
+
+namespace Custom_Exception
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                throw new UserAlreadyLoggedInException("User is already logged in");
+            }
+            catch(UserAlreadyLoggedInException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+    }
+
+    [Serializable]
+    public class UserAlreadyLoggedInException : Exception
+    {
+        public UserAlreadyLoggedInException() : base()
+        {
+        }   
+    public UserAlreadyLoggedInException(string message) : base(message)
+        {
+        }
+    public UserAlreadyLoggedInException(string message , Exception innerexp) : base(message , innerexp)
+        {
+        }
+    public UserAlreadyLoggedInException(SerializationInfo info , StreamingContext sc) : base(info , sc)
+        {
+        }
+    }       
+}
