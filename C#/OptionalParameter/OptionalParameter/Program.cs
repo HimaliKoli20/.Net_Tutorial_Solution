bed7df8ec60da3fb0f13cecf4f1f44a126087ab9
himalikoli20 <himalikoli208@gmail.com>
2020-05-07 19:10:01 +0530

    Task Added

---
+++ C#/OptionalParameter/OptionalParameter/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace OptionalParameter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            AddNum(20, 8 , 45 , 50, 78);
+        }
+
+        public static void AddNum(int Num1 , int Num2 , params object[] restOfNum) //params parameter must be the last parameter
+        {
+            int result = Num1 + Num2;
+            if(restOfNum != null)
+            {
+                foreach(int i in restOfNum)
+                {
+                    result += i;
+                }
+            }
+            Console.WriteLine("Sum = {0} ", result);
+        }
+    }
+}
