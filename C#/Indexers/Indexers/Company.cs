6cca24e777ef4f7f8898ab507f7904871b6aae80
himalikoli20 <himalikoli208@gmail.com>
2020-05-07 18:33:28 +0530

    Task Added

---
+++ C#/Indexers/Indexers/Company.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.ModelBinding;
+
+namespace Indexers
+{
+    public class Employee
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public string Gender { get; set; }
+    }
+    public class Company
+    {
+        private List<Employee> ListOfEmployees;
+
+        public Company()
+        {
+            ListOfEmployees = new List<Employee>();
+
+
+            ListOfEmployees.Add(new Employee() { ID = 1, Name = "Mike", Gender = "Male" });
+            ListOfEmployees.Add(new Employee() { ID = 2, Name = "Sara", Gender = "Female" });
+            ListOfEmployees.Add(new Employee() { ID = 3, Name = "Mary", Gender = "Female" });
+            ListOfEmployees.Add(new Employee() { ID = 4, Name = "John", Gender = "Male" });
+            ListOfEmployees.Add(new Employee() { ID = 5, Name = "Nick", Gender = "Male" });
+
+        }
+
+        public string this[int Id]
+        {
+            get
+            {
+                return ListOfEmployees.FirstOrDefault(emp => emp.ID == Id).Name;
+            }
+            set
+            {
+                ListOfEmployees.FirstOrDefault(emp => emp.ID == Id).Name = value;
+            }
+        }
+            
+
+    }
+}
