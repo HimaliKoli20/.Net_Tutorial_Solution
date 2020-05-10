61c60fdca8095c03ed1737312b6afe81b48fada5
himalikoli20 <himalikoli208@gmail.com>
2020-05-10 18:51:56 +0530

    Task Added

---
+++ C#/Multithreading/_Multithreading/Form1.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Threading;
+using System.Windows.Forms;
+
+namespace ThreadingExample
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnTimeConsumingWork_Click(object sender, EventArgs e)
+        {
+            btnTimeConsumingWork.Enabled = false;
+            btnPrintNumbers.Enabled = false;
+
+            DoTimeConsumingWork();
+
+            btnTimeConsumingWork.Enabled = true;
+            btnPrintNumbers.Enabled = true;
+        }
+
+        private void DoTimeConsumingWork()
+        {
+            // Make the thread sleep, to introduce artifical latency
+            Thread.Sleep(5000);
+        }
+
+        private void btnPrintNumbers_Click(object sender, EventArgs e)
+        {
+            for (int i = 1; i <= 10; i++)
+            {
+                listBoxNumbers.Items.Add(i);
+            }
+        }
+    }
+}
