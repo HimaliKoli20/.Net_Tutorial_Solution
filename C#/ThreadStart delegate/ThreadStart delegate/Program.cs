7619fce672f40c19dbf4f3b86f6fb8e4edb1fd88
himalikoli20 <himalikoli208@gmail.com>
2020-05-10 20:45:51 +0530

    Task Added

---
+++ C#/ThreadStart delegate/ThreadStart delegate/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Threading;
+
+
+namespace ThreadStart_Delegate
+{
+    class Program
+    {
+        public static void Main()
+        {
+            //Thread T1 = new Thread(Number.PrintNumbers);
+            //Thread T1 = new Thread(delegate() { Number.PrintNumbers(); });      //using delegate
+            //Thread T1 = new Thread(()=>Number.PrintNumbers());                  //using lambda exp
+            Thread T1 = new Thread(new ThreadStart(Number.PrintNumbers));
+
+            T1.Start();
+        }
+    }
+
+    class Number
+    {
+        public static void PrintNumbers()
+        {
+            for (int i = 1; i <= 10; i++)
+            {
+                Console.WriteLine(i);
+            }
+        }
+    }
+}
