f566115f8ec7cea39737b8ddcb9d63e311f6a443
himalikoli20 <himalikoli208@gmail.com>
2020-05-17 21:23:02 +0530

    Task added

---
+++ ADONET/ADODemo/ADODemo/WebForm1.aspx.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data.SqlClient;
+
+namespace ADODemo
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            string sql = "Select * from tblPerson";
+            string con = @"data source = DELL-PC\SQLSERVER ; database = Practice ; integrated security = SSPI";
+            SqlConnection sqlcon = new SqlConnection(con);
+            SqlCommand cmd = new SqlCommand(sql, sqlcon);
+            sqlcon.Open();
+            SqlDataReader rdr = cmd.ExecuteReader();
+            GridView1.DataSource = rdr;
+            GridView1.DataBind();
+            sqlcon.Close();
+        }
+    }
+}
