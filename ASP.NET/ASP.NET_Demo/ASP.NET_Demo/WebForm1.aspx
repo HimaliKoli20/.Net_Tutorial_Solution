5cbfe90831994d338e7408f578f45c658110bfb2
himalikoli20 <himalikoli208@gmail.com>
2020-05-19 22:45:38 +0530

    Task Added

---
+++ ASP.NET/ASP.NET_Demo/ASP.NET_Demo/WebForm1.aspx
@@ -0,0 +1,20 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="WebForm1.aspx.cs" Inherits="ASP.NET_Demo.WebForm1" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head >
+    <title></title>
+</head>
+<body>
+   
+            <form method="post" runat="server" >
+                 Company Name:<br/> 
+                <input type="text" name="CompanyName" value=""/><br/>
+                Contact Name:<br/>
+                <input type="text" name="ContactName" value=""/><br/><br/>
+                <input type="submit" value="Submit" class="submit" />
+            </form> 
+                
+</body>
+</html>
---
+++ ASP.NET/ASP.NET_Demo/ASP.NET_Demo/WebForm1.aspx.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace ASP.NET_Demo
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (IsPostBack)
+            {
+                string companyname = Request["CompanyName"];
+                string contactname = Request["ContactName"];
+                Response.Write("You entered: <br/>");
+                Response.Write("Company Name: " + @companyname + "<br/>");
+                Response.Write("Contact Name: " + @contactname);
+
+            }
+
+        }
+  
+    }
+}
