a164a566c1bf52003078218c1d27397764994215
himalikoli20 <himalikoli208@gmail.com>
2020-06-25 23:40:26 +0530

    New Project Added

---
+++ MVC/LoginPage/LoginPage/Controllers/C_LoginController.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.SqlClient;
+using System.Configuration;
+using System.Web.Mvc;
+using System.Web.UI.WebControls;
+using LoginPage.Models;
+using System.Web.Security;
+
+namespace LoginPage.Controllers
+{
+
+    public class C_LoginController : Controller
+    {
+        SqlConnection con = new SqlConnection();
+        SqlCommand cmd = new SqlCommand();
+        
+        // GET: C_Login
+        [HttpGet]
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [Authorize]
+        public ActionResult Home()
+        {
+            return View();
+        }
+
+         public ActionResult Error()
+        {
+            return View();
+        }
+        public void connectionstring()
+        {
+            con.ConnectionString = @"data source=DELL-PC\SQLSERVER;database=Practice9;integrated security=SSPI";
+        }
+        
+        [HttpPost]
+        public ActionResult Verify(M_Login details)
+        {
+            connectionstring();
+            con.Open();
+            cmd.Connection = con;
+            cmd.CommandText = "Select * from tblUsers where UserName= '" + details.UserName + "' and Password= '" + details.Password + "'";
+            SqlDataReader sqlData = cmd.ExecuteReader();
+            if(sqlData.Read())
+            {
+                FormsAuthentication.SetAuthCookie(details.UserName,false);
+                Session["user"] = details.UserName;
+                Session["Pass"] = details.Password;
+                con.Close();
+                return View("Home");
+
+            }
+            else
+            {
+                con.Close();
+                return View("Error");
+            }
+            
+        }
+    }
+}
---
+++ MVC/LoginPage/LoginPage/Models/M_Login.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace LoginPage.Models
+{
+    public class M_Login
+    {
+        public string UserName { get; set; }
+        public string Password { get; set; }
+    }
+}
