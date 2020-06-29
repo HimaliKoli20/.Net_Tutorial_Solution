d6f727c8d92b9fec7641473e337c85554b04dac8
himalikoli20 <himalikoli208@gmail.com>
2020-06-29 23:22:32 +0530

    New Project Added

---
+++ MVC/RegistrationForm/RegistrationForm/Controllers/RegistrationFormController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.SqlClient;
+using System.Configuration;
+using System.Web.Mvc;
+using RegistrationForm.Models;
+
+namespace RegistrationForm.Controllers
+{
+    public class RegistrationFormController : Controller
+    {
+        SqlConnection con = new SqlConnection();
+        SqlCommand cmd = new SqlCommand();
+        // GET: RegistrationForm
+        public ActionResult RegistrationForm()
+        {
+            return View();
+        }
+        public void connectionstring()
+        {
+            con.ConnectionString = @"data source=DELL-PC\SQLSERVER; database=RegistrationDetails; integrated security=SSPI";
+        }
+
+        [HttpPost]
+        public ActionResult Verify(RegistrationFormDetails details)
+        {
+            connectionstring();
+            con.Open();
+            cmd.Connection = con;
+            cmd.CommandText = "insert into tblCandidateDetails values ('"+details.FirstName+"','"+details.LastName+"','"+details.PhoneNumber+ "','"+details.Email+ "','"+details.DOB+"','"+details.Gender+ "','"+details.Qualification+ "','"+details.Branch+ "','"+details.YearOfPassing+ "','"+details.CurrentEmploymentStatus+ "','"+details.TotalWorkExperience+ "','"+details.InterestedJobProfiles+"')";
+            cmd.ExecuteNonQuery();
+            con.Close();
+            return View("Verify");
+            
+        }
+    }
+}
---
+++ MVC/RegistrationForm/RegistrationForm/Models/RegistrationFormDetails.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace RegistrationForm.Models
+{
+    public class RegistrationFormDetails
+    {
+        public string FirstName {get;set;}
+        public string LastName  {get;set;}
+        public string PhoneNumber { get;set;}
+        public string Email { get;set;}
+        public string DOB  { get;set;}
+        public string Gender { get;set;}
+        public string Qualification { get;set;}
+        public string Branch { get;set;}
+        public string YearOfPassing { get;set;}
+        public string CurrentEmploymentStatus { get;set;}
+        public string TotalWorkExperience { get;set;}
+        public string InterestedJobProfiles { get;set;}
+    }
+}
