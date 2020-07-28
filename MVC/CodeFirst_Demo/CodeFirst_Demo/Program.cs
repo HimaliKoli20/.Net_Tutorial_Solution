1c3ec6398c4aaad94759b7845a45a8604aa30d7c
himalikoli20 <himalikoli208@gmail.com>
2020-07-28 16:55:35 +0530

    Adding demo of code first workflow

---
+++ MVC/CodeFirst_Demo/CodeFirst_Demo/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeFirst_Demo
+{
+	public class User           //Step 1 : class to create a table and columns
+	{
+		public int Id { get; set; }
+		public string Name { get; set; }
+	}
+	public class UserDbContext : DbContext   //it is an abstraction over to the database that is used to load or save the data
+	//step 2 : to create a class that derives from DbContext and expose one or more DbSets
+	{
+		public DbSet<User> user { get; set; }
+	}
+	 //Step 3 : Go to App.config and create a connection string 
+	class Program
+	{
+		static void Main(string[] args)
+		{
+		}
+	}
+}
