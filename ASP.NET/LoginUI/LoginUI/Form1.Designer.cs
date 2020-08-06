6a8e0e84517d2868f893f0158cabf20f7aecdddc
himalikoli20 <himalikoli208@gmail.com>
2020-08-06 18:36:06 +0530

    Complete Login UI

---
+++ ASP.NET/LoginUI/LoginUI/Form1.Designer.cs
@@ -0,0 +1,278 @@
+﻿namespace LoginUI
+{
+	partial class LoginFormUI
+	{
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.components = new System.ComponentModel.Container();
+			System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(LoginFormUI));
+			this.bunifuElipse1 = new Bunifu.Framework.UI.BunifuElipse(this.components);
+			this.bunifuGradientPanel1 = new Bunifu.Framework.UI.BunifuGradientPanel();
+			this.label3 = new System.Windows.Forms.Label();
+			this.bunifuThinButton21 = new Bunifu.Framework.UI.BunifuThinButton2();
+			this.label2 = new System.Windows.Forms.Label();
+			this.pictureBox3 = new System.Windows.Forms.PictureBox();
+			this.label1 = new System.Windows.Forms.Label();
+			this.bunifuCheckbox1 = new Bunifu.Framework.UI.BunifuCheckbox();
+			this.panel2 = new System.Windows.Forms.Panel();
+			this.pictureBox2 = new System.Windows.Forms.PictureBox();
+			this.alphaBlendTextBox2 = new ZBobb.AlphaBlendTextBox();
+			this.panel1 = new System.Windows.Forms.Panel();
+			this.pictureBox1 = new System.Windows.Forms.PictureBox();
+			this.alphaBlendTextBox1 = new ZBobb.AlphaBlendTextBox();
+			this.bunifuGradientPanel1.SuspendLayout();
+			((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+			((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+			((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+			this.SuspendLayout();
+			// 
+			// bunifuElipse1
+			// 
+			this.bunifuElipse1.ElipseRadius = 20;
+			this.bunifuElipse1.TargetControl = this;
+			// 
+			// bunifuGradientPanel1
+			// 
+			this.bunifuGradientPanel1.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("bunifuGradientPanel1.BackgroundImage")));
+			this.bunifuGradientPanel1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+			this.bunifuGradientPanel1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+			this.bunifuGradientPanel1.Controls.Add(this.label3);
+			this.bunifuGradientPanel1.Controls.Add(this.bunifuThinButton21);
+			this.bunifuGradientPanel1.Controls.Add(this.label2);
+			this.bunifuGradientPanel1.Controls.Add(this.pictureBox3);
+			this.bunifuGradientPanel1.Controls.Add(this.label1);
+			this.bunifuGradientPanel1.Controls.Add(this.bunifuCheckbox1);
+			this.bunifuGradientPanel1.Controls.Add(this.panel2);
+			this.bunifuGradientPanel1.Controls.Add(this.pictureBox2);
+			this.bunifuGradientPanel1.Controls.Add(this.alphaBlendTextBox2);
+			this.bunifuGradientPanel1.Controls.Add(this.panel1);
+			this.bunifuGradientPanel1.Controls.Add(this.pictureBox1);
+			this.bunifuGradientPanel1.Controls.Add(this.alphaBlendTextBox1);
+			this.bunifuGradientPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+			this.bunifuGradientPanel1.ForeColor = System.Drawing.Color.Transparent;
+			this.bunifuGradientPanel1.GradientBottomLeft = System.Drawing.Color.RoyalBlue;
+			this.bunifuGradientPanel1.GradientBottomRight = System.Drawing.Color.Orchid;
+			this.bunifuGradientPanel1.GradientTopLeft = System.Drawing.Color.Orchid;
+			this.bunifuGradientPanel1.GradientTopRight = System.Drawing.Color.MediumPurple;
+			this.bunifuGradientPanel1.Location = new System.Drawing.Point(0, 0);
+			this.bunifuGradientPanel1.Name = "bunifuGradientPanel1";
+			this.bunifuGradientPanel1.Quality = 10;
+			this.bunifuGradientPanel1.Size = new System.Drawing.Size(400, 650);
+			this.bunifuGradientPanel1.TabIndex = 10;
+			this.bunifuGradientPanel1.Paint += new System.Windows.Forms.PaintEventHandler(this.bunifuGradientPanel1_Paint);
+			// 
+			// label3
+			// 
+			this.label3.AutoSize = true;
+			this.label3.BackColor = System.Drawing.Color.Transparent;
+			this.label3.Font = new System.Drawing.Font("Century Gothic", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.label3.ForeColor = System.Drawing.Color.LightGray;
+			this.label3.Location = new System.Drawing.Point(122, 606);
+			this.label3.Name = "label3";
+			this.label3.Size = new System.Drawing.Size(155, 20);
+			this.label3.TabIndex = 11;
+			this.label3.Text = "FORGOT PASSWORD";
+			// 
+			// bunifuThinButton21
+			// 
+			this.bunifuThinButton21.ActiveBorderThickness = 1;
+			this.bunifuThinButton21.ActiveCornerRadius = 40;
+			this.bunifuThinButton21.ActiveFillColor = System.Drawing.Color.White;
+			this.bunifuThinButton21.ActiveForecolor = System.Drawing.Color.DimGray;
+			this.bunifuThinButton21.ActiveLineColor = System.Drawing.Color.White;
+			this.bunifuThinButton21.BackColor = System.Drawing.Color.Transparent;
+			this.bunifuThinButton21.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("bunifuThinButton21.BackgroundImage")));
+			this.bunifuThinButton21.ButtonText = "Login";
+			this.bunifuThinButton21.Cursor = System.Windows.Forms.Cursors.Hand;
+			this.bunifuThinButton21.Font = new System.Drawing.Font("Century Gothic", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.bunifuThinButton21.ForeColor = System.Drawing.Color.DimGray;
+			this.bunifuThinButton21.IdleBorderThickness = 1;
+			this.bunifuThinButton21.IdleCornerRadius = 45;
+			this.bunifuThinButton21.IdleFillColor = System.Drawing.Color.White;
+			this.bunifuThinButton21.IdleForecolor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+			this.bunifuThinButton21.IdleLineColor = System.Drawing.Color.White;
+			this.bunifuThinButton21.Location = new System.Drawing.Point(103, 472);
+			this.bunifuThinButton21.Margin = new System.Windows.Forms.Padding(6, 5, 6, 5);
+			this.bunifuThinButton21.Name = "bunifuThinButton21";
+			this.bunifuThinButton21.Size = new System.Drawing.Size(193, 62);
+			this.bunifuThinButton21.TabIndex = 0;
+			this.bunifuThinButton21.Tag = "";
+			this.bunifuThinButton21.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+			this.bunifuThinButton21.Click += new System.EventHandler(this.bunifuThinButton21_Click);
+			// 
+			// label2
+			// 
+			this.label2.AutoSize = true;
+			this.label2.BackColor = System.Drawing.Color.Transparent;
+			this.label2.Font = new System.Drawing.Font("Century Gothic", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.label2.ForeColor = System.Drawing.Color.White;
+			this.label2.Location = new System.Drawing.Point(158, 127);
+			this.label2.Name = "label2";
+			this.label2.Size = new System.Drawing.Size(85, 25);
+			this.label2.TabIndex = 9;
+			this.label2.Text = "LOG IN";
+			this.label2.Click += new System.EventHandler(this.label2_Click);
+			// 
+			// pictureBox3
+			// 
+			this.pictureBox3.BackColor = System.Drawing.Color.Transparent;
+			this.pictureBox3.Image = global::LoginUI.Properties.Resources.Logo6;
+			this.pictureBox3.Location = new System.Drawing.Point(159, 32);
+			this.pictureBox3.Name = "pictureBox3";
+			this.pictureBox3.Size = new System.Drawing.Size(81, 70);
+			this.pictureBox3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+			this.pictureBox3.TabIndex = 8;
+			this.pictureBox3.TabStop = false;
+			// 
+			// label1
+			// 
+			this.label1.AutoSize = true;
+			this.label1.BackColor = System.Drawing.Color.Transparent;
+			this.label1.Font = new System.Drawing.Font("Century Gothic", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.label1.ForeColor = System.Drawing.Color.LightGray;
+			this.label1.Location = new System.Drawing.Point(69, 390);
+			this.label1.Name = "label1";
+			this.label1.Size = new System.Drawing.Size(116, 20);
+			this.label1.TabIndex = 7;
+			this.label1.Text = "Remember me";
+			// 
+			// bunifuCheckbox1
+			// 
+			this.bunifuCheckbox1.BackColor = System.Drawing.Color.White;
+			this.bunifuCheckbox1.ChechedOffColor = System.Drawing.Color.White;
+			this.bunifuCheckbox1.Checked = true;
+			this.bunifuCheckbox1.CheckedOnColor = System.Drawing.Color.White;
+			this.bunifuCheckbox1.ForeColor = System.Drawing.Color.DarkGray;
+			this.bunifuCheckbox1.Location = new System.Drawing.Point(35, 390);
+			this.bunifuCheckbox1.Name = "bunifuCheckbox1";
+			this.bunifuCheckbox1.Size = new System.Drawing.Size(20, 20);
+			this.bunifuCheckbox1.TabIndex = 6;
+			// 
+			// panel2
+			// 
+			this.panel2.Location = new System.Drawing.Point(35, 362);
+			this.panel2.Name = "panel2";
+			this.panel2.Size = new System.Drawing.Size(335, 2);
+			this.panel2.TabIndex = 5;
+			// 
+			// pictureBox2
+			// 
+			this.pictureBox2.BackColor = System.Drawing.Color.Transparent;
+			this.pictureBox2.Image = global::LoginUI.Properties.Resources.lock2;
+			this.pictureBox2.Location = new System.Drawing.Point(35, 314);
+			this.pictureBox2.Name = "pictureBox2";
+			this.pictureBox2.Size = new System.Drawing.Size(32, 32);
+			this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+			this.pictureBox2.TabIndex = 4;
+			this.pictureBox2.TabStop = false;
+			// 
+			// alphaBlendTextBox2
+			// 
+			this.alphaBlendTextBox2.BackAlpha = 0;
+			this.alphaBlendTextBox2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
+			this.alphaBlendTextBox2.BorderStyle = System.Windows.Forms.BorderStyle.None;
+			this.alphaBlendTextBox2.Font = new System.Drawing.Font("Century Gothic", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.alphaBlendTextBox2.ForeColor = System.Drawing.Color.White;
+			this.alphaBlendTextBox2.Location = new System.Drawing.Point(73, 314);
+			this.alphaBlendTextBox2.Multiline = true;
+			this.alphaBlendTextBox2.Name = "alphaBlendTextBox2";
+			this.alphaBlendTextBox2.Size = new System.Drawing.Size(294, 48);
+			this.alphaBlendTextBox2.TabIndex = 3;
+			this.alphaBlendTextBox2.Text = "Password";
+			this.alphaBlendTextBox2.TextChanged += new System.EventHandler(this.alphaBlendTextBox2_TextChanged);
+			// 
+			// panel1
+			// 
+			this.panel1.Location = new System.Drawing.Point(35, 275);
+			this.panel1.Name = "panel1";
+			this.panel1.Size = new System.Drawing.Size(335, 2);
+			this.panel1.TabIndex = 2;
+			// 
+			// pictureBox1
+			// 
+			this.pictureBox1.BackColor = System.Drawing.Color.Transparent;
+			this.pictureBox1.Image = global::LoginUI.Properties.Resources.user;
+			this.pictureBox1.Location = new System.Drawing.Point(35, 223);
+			this.pictureBox1.Name = "pictureBox1";
+			this.pictureBox1.Size = new System.Drawing.Size(32, 33);
+			this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+			this.pictureBox1.TabIndex = 1;
+			this.pictureBox1.TabStop = false;
+			// 
+			// alphaBlendTextBox1
+			// 
+			this.alphaBlendTextBox1.BackAlpha = 0;
+			this.alphaBlendTextBox1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
+			this.alphaBlendTextBox1.BorderStyle = System.Windows.Forms.BorderStyle.None;
+			this.alphaBlendTextBox1.Font = new System.Drawing.Font("Century Gothic", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.alphaBlendTextBox1.ForeColor = System.Drawing.Color.White;
+			this.alphaBlendTextBox1.Location = new System.Drawing.Point(73, 223);
+			this.alphaBlendTextBox1.Multiline = true;
+			this.alphaBlendTextBox1.Name = "alphaBlendTextBox1";
+			this.alphaBlendTextBox1.Size = new System.Drawing.Size(294, 52);
+			this.alphaBlendTextBox1.TabIndex = 0;
+			this.alphaBlendTextBox1.Text = "Username";
+			this.alphaBlendTextBox1.TextChanged += new System.EventHandler(this.alphaBlendTextBox1_TextChanged);
+			// 
+			// LoginFormUI
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(400, 650);
+			this.Controls.Add(this.bunifuGradientPanel1);
+			this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+			this.Name = "LoginFormUI";
+			this.Text = "Login Form";
+			this.Load += new System.EventHandler(this.LoginFormUI_Load);
+			this.bunifuGradientPanel1.ResumeLayout(false);
+			this.bunifuGradientPanel1.PerformLayout();
+			((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+			((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+			((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+			this.ResumeLayout(false);
+
+		}
+
+		#endregion
+
+		private Bunifu.Framework.UI.BunifuGradientPanel bunifuGradientPanel1;
+		private ZBobb.AlphaBlendTextBox alphaBlendTextBox1;
+		private System.Windows.Forms.PictureBox pictureBox1;
+		private System.Windows.Forms.Panel panel2;
+		private ZBobb.AlphaBlendTextBox alphaBlendTextBox2;
+		private System.Windows.Forms.Panel panel1;
+		private System.Windows.Forms.PictureBox pictureBox2;
+		private Bunifu.Framework.UI.BunifuElipse bunifuElipse1;
+		private Bunifu.Framework.UI.BunifuCheckbox bunifuCheckbox1;
+		private System.Windows.Forms.PictureBox pictureBox3;
+		private System.Windows.Forms.Label label1;
+		private System.Windows.Forms.Label label2;
+		private Bunifu.Framework.UI.BunifuThinButton2 bunifuThinButton21;
+		private System.Windows.Forms.Label label3;
+	}
+}
+
